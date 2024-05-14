07880917617a76214b758e87b15632b38ba34878
Jules Jean-Louis <jules_jean-louis@natural-solutions.eu>
2024-05-14 14:59:11 +0200

    feat: wip autocomplete

---
+++ app/src/app/components/shared/Autocomplete.tsx
@@ -0,0 +1,52 @@
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+
+export const Autocomplete: React.FC = () => {
+  const [cities, setCities] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const [selectedCity, setSelectedCity] = useState<any>("");
+
+  const handleSearch = (e: any) => {
+    setSearch(e.target.value);
+  }
+
+  const handleCity = (e: any) => {
+    setSelectedCity(e.target.value);
+  }
+
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch("/api/city");
+      const data = await resp.json();
+      setCities(data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (search === "") return;
+    console.log(selectedCity)
+    console.log(search);
+    (async () => {
+        const resp = await fetch(`/api/restaurant/search/${selectedCity}/${search}`);
+        const data = await resp.json();
+        console.log(data);
+    })();
+  }, [search]);
+
+  return (
+    <>
+      <div className="rounded-lg border border-green-100">
+        <div className="flex items-center">
+            <select className="w-40" onChange={(e) =>(handleCity(e))}>
+                <option value="">Ville</option>
+                {cities.map((city: any) => {
+                    return <option key={city.id} value={city.id}>{city.name}</option>;
+                })}
+            </select>
+            <Input placeholder="Rechercher un restaurant" onChange={(e) => (handleSearch(e))} />
+        </div>
+      </div>
+    </>
+  );
+};
--- app/src/app/components/shared/Header.tsx
+++ app/src/app/components/shared/Header.tsx
@@ -4,25 +4,48 @@ import { Button } from "@/components/ui/button";
 import { UserRound } from "lucide-react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-
+import { Autocomplete } from "./Autocomplete";
 
 export const Header = () => {
-  const {data} = useSession();
+  const { data } = useSession();
 
   return (
-    <header className="w-screen h-14 p-3">
+    <header className="w-screen h-14 lg:py-3 lg:px-2 border border-b-green-200">
       <div className="w-full flex justify-between">
-        <div>
-          <Link href="/">Home</Link>
+        <div className="flex items-center space-x-2">
+          <div>
+            <Link href="/">Miam</Link>
+          </div>
+          <nav className="flex">
+            <ul className="flex items-center space-x-2">
+              <li>
+                <Link href="/restaurants">
+                  <h6>Restaurants</h6>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  <h6>A propos</h6>
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
+        <Autocomplete />
         <div>
           {data ? (
-            <Button className="flex justify-center">
+            <Button
+              className="flex justify-center max-h-10 space-x-2"
+              size="sm"
+            >
               <UserRound />
               <Link href="/profile">{data.user.firstname}</Link>
             </Button>
           ) : (
-            <Button className="flex justify-center">
+            <Button
+              className="flex justify-center max-h-10 space-x-2"
+              size="sm"
+            >
               <UserRound />
               <Link href="/login">Connexion</Link>
             </Button>
