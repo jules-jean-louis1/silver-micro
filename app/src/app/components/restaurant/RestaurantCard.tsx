cc906636d1afe12c930d8cd5c279f446f7d0a60e
Jules Jean-Louis <jules_jean-louis@natural-solutions.eu>
2024-05-18 11:39:10 +0200

    fix: restaurant card

--- app/src/app/components/restaurant/RestaurantCard.tsx
+++ app/src/app/components/restaurant/RestaurantCard.tsx
@@ -1,40 +1,37 @@
 import { Button } from "@/components/ui/button";
-import { Link } from "lucide-react";
+import { MoveRight } from "lucide-react";
+import Link from "next/link";
 import { FC } from "react";
 
 interface RestaurantCardProps {
   restaurants: any;
 }
 export const RestaurantCard: FC<RestaurantCardProps> = ({ restaurants }) => {
-  console.log(restaurants);
+
   return (
     <article className="lg:w-[48%] lg:min-h-[200px] p-3 flex border border-green-olive rounded-lg shadow-md space-x-4">
       <div className="h-full w-1/3 bg-slate-500"></div>
       <div className="flex flex-col justify-between w-full items-start">
         <p>
-          {restaurants.frame_ambiances?.length > 0
-            ? restaurants.frame_ambiances[0].name
+          {restaurants.frame_ambiences?.length > 0
+            ? restaurants.frame_ambiences[0].name
             : ""}{" "}
-          {restaurants.dishes?.length > 0
-            ? "- " + restaurants.dishes[0].name
+          {restaurants.cooking_types?.length > 0
+            ? "- " + restaurants.cooking_types[0].name
             : ""}{" "}
         </p>
-        <h3>{restaurants.name}</h3>
+        <h3 className="text-xl font-semibold">{restaurants.name}</h3>
         <p>
           {restaurants.address} ,{" "}
           {restaurants.cities?.length > 0 ? restaurants.cities[0].name : ""}
         </p>
-        <p>
-          {restaurants.cooking_types?.length > 0
-            ? restaurants.cooking_types[0].name
-            : ""}
-        </p>
         <div className="flex justify-center">
           <Button
             size="sm"
-            className="bg-green-olive text-flora-white min-w-28"
+            className="flex items-center space-x-3 bg-green-olive text-flora-white hover:bg-ecalyptus-green lg:min-w-40 font-medium"
           >
             <Link href={`/restaurant/${restaurants.id}`}>Réserver</Link>
+            <MoveRight />
           </Button>
         </div>
       </div>
